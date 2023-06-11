67a74f1423f3cdcda17043c26f460e37df9d9e1b
Mahak Gupta <mahak03.gupta@gmail.com>
2023-06-11 11:41:54 +0530

    update

--- selfstudy/settings.py
+++ selfstudy/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mhkselfstudy.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
